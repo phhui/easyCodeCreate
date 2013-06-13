a66a29576fcf66ddfdec137428a03755dde394d0
phhui <phhui@qq.com>
2013-06-13 11:37:38 +0800

    小菜简易代码生成器
    写功能模块已基本成型，每次写个新模块都有一些基本的结构需要创建，挺浪费时间的，抽了点时间写了个自动生成的，生成由模板直接生成，暂时只是简单文本替换而已。后续根据需求完善。

---
+++ CodeCreate/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace CodeCreate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_modelName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_path = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_createCode = new System.Windows.Forms.Button();
+            this.txt_output = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "模块名称：";
+            // 
+            // txt_modelName
+            // 
+            this.txt_modelName.Location = new System.Drawing.Point(66, 32);
+            this.txt_modelName.MaxLength = 30;
+            this.txt_modelName.Name = "txt_modelName";
+            this.txt_modelName.Size = new System.Drawing.Size(205, 21);
+            this.txt_modelName.TabIndex = 1;
+            this.txt_modelName.Text = "必需为英文字母";
+            this.txt_modelName.WordWrap = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "存储位置：";
+            // 
+            // txt_path
+            // 
+            this.txt_path.Location = new System.Drawing.Point(66, 5);
+            this.txt_path.Name = "txt_path";
+            this.txt_path.ReadOnly = true;
+            this.txt_path.Size = new System.Drawing.Size(697, 21);
+            this.txt_path.TabIndex = 5;
+            this.txt_path.Text = "点击选择路径,项目中类路径默认为main.submodules";
+            this.txt_path.Click += new System.EventHandler(this.txt_path_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(273, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(11, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "*";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(763, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(11, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "*";
+            // 
+            // btn_createCode
+            // 
+            this.btn_createCode.Location = new System.Drawing.Point(289, 30);
+            this.btn_createCode.Name = "btn_createCode";
+            this.btn_createCode.Size = new System.Drawing.Size(75, 23);
+            this.btn_createCode.TabIndex = 22;
+            this.btn_createCode.Text = "生  成";
+            this.btn_createCode.UseVisualStyleBackColor = true;
+            this.btn_createCode.Click += new System.EventHandler(this.btn_createCode_Click);
+            // 
+            // txt_output
+            // 
+            this.txt_output.Location = new System.Drawing.Point(8, 59);
+            this.txt_output.Multiline = true;
+            this.txt_output.Name = "txt_output";
+            this.txt_output.Size = new System.Drawing.Size(766, 491);
+            this.txt_output.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.txt_output);
+            this.Controls.Add(this.btn_createCode);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_path);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_modelName);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "小菜简单代码生成器 by phhui";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_modelName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_path;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_createCode;
+        private System.Windows.Forms.TextBox txt_output;
+    }
+}
+
---
+++ CodeCreate/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CodeCreate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private List<FileInfo> fileListData = new List<FileInfo>(); //保存所有的文件信息 
+        private void btn_createCode_Click(object sender, EventArgs e)
+        {
+            if (txt_modelName.Text.Length < 3)
+            {
+                MessageBox.Show("请输入项目名字");
+                return;
+            }
+            if (txt_path.Text.Length < 5 || txt_path.Text == "点击选择路径,项目中类路径默认为main.submodules")
+            {
+                MessageBox.Show("请选择项目位置");
+                return;
+            }
+            GetAllFilesInDirectory("template/", true);
+            createFile();
+        }
+        private void txt_path_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fb = new FolderBrowserDialog();
+            if (fb.ShowDialog() == DialogResult.OK)
+            {
+                txt_path.Text = fb.SelectedPath;
+            }
+        }
+        public void GetAllFilesInDirectory(string strDirectory, Boolean contains)
+        {
+            if (!Directory.Exists(strDirectory))
+            {
+                MessageBox.Show("模板路径无效~！");
+                return;
+            }
+            DirectoryInfo directory = new DirectoryInfo(strDirectory);
+            DirectoryInfo[] directoryArray = directory.GetDirectories();
+            FileInfo[] fileInfoArray = directory.GetFiles();
+            if (fileInfoArray.Length > 0) fileListData.AddRange(fileInfoArray);
+            if (contains)
+            {
+                foreach (DirectoryInfo _directoryInfo in directoryArray)
+                {//遍历子目录
+                    GetAllFilesInDirectory(_directoryInfo.FullName, contains);
+                }
+            }
+            txt_output.Text = "获取模板数据~~\r\n";            
+        }
+        private void createFile()
+        {
+            foreach (FileInfo fi in fileListData)
+            {
+                string p = fi.FullName.Replace(System.Windows.Forms.Application.StartupPath + "\\template\\", "").Replace(fi.Name, "");//模板目录结构
+                string path = txt_path.Text + "/" + txt_modelName.Text + "/" + p;//目标目录
+                Directory.CreateDirectory(path);//创建目标目录
+                string str = File.ReadAllText(fi.FullName);//读取模板数据
+                str = str.Replace("56lea.com", txt_modelName.Text);//替换模板数据
+                txt_output.Text += fi.Name.Replace("56lea.com",txt_modelName.Text) + "生成完毕，位置：" + path + fi.Name.Replace("56lea.com", txt_modelName.Text).Replace("/","\\") + "\r\n";
+                string fileType = fi.Name.Substring(fi.Name.Length - 4);
+                if (fileType.IndexOf(".jpg") != -1 || fileType.IndexOf(".gif") != -1 || fileType.IndexOf(".png") != -1 || fileType.IndexOf(".jpeg") != -1 || fileType.IndexOf(".swf") != -1)
+                {
+                    File.Copy(fi.FullName, path + fi.Name.Replace("56lea.com", txt_modelName.Text));
+                }
+                else
+                {
+                    File.WriteAllText(path + fi.Name.Replace("56lea.com", txt_modelName.Text), str);//生成代码
+                }
+            }
+            txt_output.Text += "生成完毕~~";
+            MessageBox.Show("代码生成完毕");
+        }
+    }
+}
